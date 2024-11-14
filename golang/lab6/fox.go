9b11ebd1861b2f4cc9f80cfa2f3005f49c38bf02
GypsyGang37 <bobartem04@gmail.com>
2024-11-14 19:36:14 +0300

    Update fox.go

--- golang/lab6/fox.go
+++ golang/lab6/fox.go
@@ -4,24 +4,20 @@ import (
 	"fmt"
 )
 
-// Fox представляет лисицу с определенными характеристиками.
 type Fox struct {
 	Name  string
 	Color string
 	Age   int
 }
 
-// Speak возвращает строку, представляющую звук, который издает лисица.
 func (f *Fox) Speak() string {
 	return "Ring-ding-ding-ding-dingeringeding!"
 }
 
-// GetDescription возвращает описание лисицы.
 func (f *Fox) GetDescription() string {
 	return fmt.Sprintf("This is %s, a %d-year-old %s fox.", f.Name, f.Age, f.Color)
 }
 
-// GetView возвращает ASCII-изображение лисицы.
 func (f *Fox) GetView() string {
 	return ` 
          \\ // 
