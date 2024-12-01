2239ab7b989ce4835ae2ddc9e22e097e5288a709
GypsyGang37 <bobartem04@gmail.com>
2024-12-01 18:32:23 +0300

    lab6

--- golang/lab6/fox.go
+++ golang/lab6/fox.go
@@ -11,26 +11,38 @@ type Fox struct {
 }
 
 func (f *Fox) Speak() string {
-	return "Ring-ding-ding-ding-dingeringeding!"
+	return "Rrrr-rrrr-rrrr-rrrr-rraavv!"
 }
 
 func (f *Fox) GetDescription() string {
-	return fmt.Sprintf("This is %s, a %d-year-old %s fox.", f.Name, f.Age, f.Color)
+	return fmt.Sprintf("Это %s, ей %d года %s лиса.", f.Name, f.Age, f.Color)
 }
 
 func (f *Fox) GetView() string {
 	return ` 
          \\ // 
         ( o.o )
-         > ^ <
+         > ^ < 
       `
+}
+
+func (f *Fox) IsAdult() bool {
+	return f.Age >= 2
 }
 
 func RunLab6() {
 	fox := Fox{
 		Name:  "Foxy",
-		Color: "red",
 		Age:   3,
+		Color: "красная",
+	}
+
+	fmt.Println(fox.GetDescription())
+
+	if fox.IsAdult() {
+		fmt.Println("Эта лиса - взрослая.")
+	} else {
+		fmt.Println("Эта лиса - не взрослая.")
 	}
 
 	fmt.Println(fox.Speak())
