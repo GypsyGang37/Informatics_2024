98a99dadced1e0039ceca9e39a9fc340890f57fb
GypsyGang37 <bobartem04@gmail.com>
2024-12-09 11:47:59 +0300

    lab7

--- golang/main.go
+++ golang/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	"isuct.ru/informatics2022/lab4"
-	"isuct.ru/informatics2022/lab6"
+	//"isuct.ru/informatics2022/lab4"
+	//"isuct.ru/informatics2022/lab6"
+	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
-	lab4.Run4()
-	lab6.RunLab6()
+	//lab4.Run4()
+	//lab6.RunLab6()
+	lab7.Run7()
 	fmt.Println("Бобков Артём Андреевич")
 }
